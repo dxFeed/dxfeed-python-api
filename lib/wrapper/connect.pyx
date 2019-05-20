fd273d74805e2d8bd399a5e72e08f509ae2799d4
Ivan Mishalkin <iamishalkin@gmail.com>
2019-05-20 17:27:01 +0300

    [EN-137] array of syms

--- lib/wrapper/connect.pyx
+++ lib/wrapper/connect.pyx
@@ -22,12 +22,22 @@ def pysubscribe():
 cdef extern from "Python.h":
     dxf_const_string_t PyUnicode_AsWideCharString(object, Py_ssize_t *)
 
-
+# import numpy as np
+# cimport numpy as np
 def py_add_symbol():
     my_string = u"AAPL"
+    # for i in range(len(symbols)):
     cdef Py_ssize_t length
-    cdef wchar_t *c_symbols = PyUnicode_AsWideCharString(my_string, &length)
-    print(f"{dxf_add_symbol(subscription, c_symbols)}")
+    cdef dxf_const_string_t c_symbols = PyUnicode_AsWideCharString(my_string, &length)
+    my_string2 = u"MSFT"
+    cdef Py_ssize_t length2
+    cdef dxf_const_string_t c_symbols2 = PyUnicode_AsWideCharString(my_string2, &length)
+    # c_symbols = np.array(symbols)
+    cdef dxf_const_string_t c_syms[2]
+    c_syms[:] = [c_symbols, c_symbols2]
+    #print(c_syms)
+    # c_symbols[1] = my_string2
+    print(f"{dxf_add_symbols(subscription, c_syms, 2)}")
     print('added')
 
 cdef extern from "wchar.h":
@@ -39,26 +49,23 @@ def py_get_smth():
     cdef wchar_t *c_symbols = PyUnicode_AsWideCharString(my_string, &length)
     cdef dxf_event_data_t data
     cdef dxf_trade_t *trade
-    print(f"{dxf_get_last_event(connection, 1, c_symbols, &data)}")
-    print('Got')
+    n=10
+    import time
+    for _ in range(n):
+        # event type in EventData.h (e.g. #define DXF_ET_TRADE         (1 << dx_eid_trade))
+        dxf_get_last_event(connection, 1, c_symbols, &data)
+        if data:
+            trade = <dxf_trade_t*?>data
+            print(f"time {trade.time}")
+            print(f"ex code {trade.exchange_code}")
 
+            print(f"{trade.price}")
+            print(f"{trade.size}")
+            print(f"{trade.tick}")
+            print('if done')
+        print('final')
+        time.sleep(2)
 
-    if data:
-        trade = <dxf_trade_t*>data
-        print(f"time {trade.time}")
-        print(f"ex code {trade.exchange_code}")
-
-
-        #wprintf("\nSymbol: %s; time=%lld, exchange code=%C, price=%f, size=%ld, tick=%ld, change=%f, day volume=%f, scope=%d\n",
-        #        c_symbols, trade.time, trade.exchange_code, trade.price, trade.size, trade.tick, trade.change,
-        #        trade.day_volume, <int>trade.scope)
-
-        print(f"{trade.exchange_code}")
-        print(f"{trade.price}")
-        print(f"{trade.size}")
-        print(f"{trade.tick}")
-        print('if done')
-    print('final')
 
 def all_in_one():
     pyconnect()
