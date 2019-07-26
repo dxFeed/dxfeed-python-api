b66b7919c193f7b1c5f8d77cb2c197fec8db34ae
Ivan Mishalkin <iamishalkin@gmail.com>
2019-07-26 14:58:00 +0300

    [EN-137] new run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 rm -rf  build
 rm lib/wrapper/*.c
 rm lib/wrapper/*.pyd
+rm $(find lib/wrapper/utils/ -name "*.c" -not -name "LinkedList.c")
+rm lib/wrapper/utils/*.pyd
 python setup.py build_ext --inplace
