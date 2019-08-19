deb4d0bbcac48e99ff26cfd24086236f888f54f4
Ivan Mishalkin <iamishalkin@gmail.com>
2019-08-19 18:21:06 +0300

    [EN-137] working example

--- lib/wrapper/Listeners/listener.pyx
+++ lib/wrapper/Listeners/listener.pyx
@@ -1,4 +1,3 @@
-from libc.stdio cimport printf
 from lib.wrapper.utils.helpers cimport *
 
 
@@ -9,8 +8,7 @@ cdef void trade_default_listener(int event_type, dxf_const_string_t symbol_name,
 
     cdef dxf_trade_t* trades = <dxf_trade_t*>data
     cdef dict py_data = <dict>user_data
-    
-    # printf("{symbol=%s, ", symbol_name)
+
     for i in range(data_count):
         py_data['data'].append([unicode_from_dxf_const_string_t(symbol_name),
                                 trades[i].price,
@@ -22,9 +20,6 @@ cdef void trade_default_listener(int event_type, dxf_const_string_t symbol_name,
                                 trades[i].time
                                 ]
                                )
-        # printf(", exchangeCode=%c, price=%f, size=%i, tick=%i, change=%f, day volume=%.0f}\n",
-        #         trades[i].exchange_code, trades[i].price, trades[i].size, trades[i].tick, trades[i].change,
-        #     trades[i].day_volume)
 
 QUOTE_COLUMNS = [
     'Symbol', 'BidTime', 'BidExchangeCode', 'BidPrice', 'BidSize',
