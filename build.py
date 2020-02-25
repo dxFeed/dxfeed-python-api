da4e3ff995cd80d4dd60031dfb002fdcbbd2a7d7
Ivan Mishalkin <iamishalkin@gmail.com>
2020-02-25 14:04:10 +0300

    [EN-1047] fixed issue

--- build.py
+++ build.py
@@ -1,51 +1,50 @@
-from setuptools import Extension, dist
-dist.Distribution().fetch_build_eggs(['cython'])
+from setuptools import Extension, find_packages
 from Cython.Build import cythonize
 from pathlib import Path
 import platform
 
+SRC_DIR='dxpyfeed'
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    use_cython = False
+    ext = 'c'
+    ext_pp = 'cpp'
+else:
+    use_cython = True
+    ext = ext_pp = 'pyx'
+
+# Get all dxfeed c api c files to be compiled into separate lib
 source_files_directory = Path(__file__).resolve().parent.joinpath('dxpyfeed', 'dxfeed-c-api', 'src')
 source_files_paths = [str(path) for path in source_files_directory.glob('*.c')]
-libs = []
+libs = list()
 if platform.system() == 'Windows':
     source_files_paths.remove(str(source_files_directory.joinpath('Linux.c')))
     libs = ['ws2_32']
 else:
     source_files_paths.remove(str(source_files_directory.joinpath('Win32.c')))
 
-package_c_files_dir = Path(__file__).resolve().parent.joinpath('dxpyfeed', 'wrapper')
-package_c_files_paths = [str(path) for path in package_c_files_dir.glob('**/*.c')]
+# Separate dxfeed c api library
+dxfeed_c = ('dxfeed_c', {'sources': source_files_paths,
+                         'libraries': libs})
 
-SRC_DIR = 'dxpyfeed'
-PACKAGES = [SRC_DIR]
+extensions = [Extension('dxpyfeed.wrapper.utils.helpers', ['dxpyfeed/wrapper/utils/helpers.' + ext]),
+              Extension('dxpyfeed.wrapper.listeners.listener', ['dxpyfeed/wrapper/listeners/listener.' + ext]),
+              Extension('dxpyfeed.wrapper.DXFeedPy', ['dxpyfeed/wrapper/DXFeedPy.' + ext_pp], libraries=libs)]
 
-dxfeed = ('dxfeed', {'sources': source_files_paths,
-                     'libraries': libs})
+if use_cython:
+    extensions = cythonize(extensions, language_level=3)
 
-ext_lis = Extension(name=SRC_DIR + '.wrapper.listeners.listener',
-                    sources=[SRC_DIR + '/wrapper/listeners/listener.pyx'],
-                    libraries=libs)
-
-ext_helpers = Extension(name=SRC_DIR + '.wrapper.utils.helpers',
-                        sources=[SRC_DIR + '/wrapper/utils/helpers.pyx'],
-                        libraries=libs)
-
-ext_dxfeed = Extension(name=SRC_DIR + '.wrapper.DXFeedPy',
-                       sources=[SRC_DIR + '/wrapper/DXFeedPy.pyx'] + package_c_files_paths,
-                       libraries=libs)
-
-EXTENSIONS = [ext_lis,
-              ext_helpers,
-              ext_dxfeed]
-
+package_data = {'': ['*']}
 
 def build(setup_kwargs):
     setup_kwargs.update({
-        'ext_modules': cythonize(EXTENSIONS, language_level=3),
+        'ext_modules': extensions,
         'zip_safe': False,
-        'libraries': [dxfeed],
+        'libraries': [dxfeed_c],
+        'packages': find_packages(),
+        'package_data': package_data,
         'include_dirs': [SRC_DIR + '/dxfeed-c-api/include/',
-                         SRC_DIR + '/dxfeed-c-api/src',
-                         SRC_DIR + '/wrapper/pxd_include',
-                         SRC_DIR + '/wrapper/utils']
+                         SRC_DIR + '/dxfeed-c-api/src']
     })
