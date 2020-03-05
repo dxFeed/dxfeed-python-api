741001c6653d55b06d59251be96c0aeab37b283a
Ivan Mishalkin <iamishalkin@gmail.com>
2020-03-05 16:55:29 +0300

    [EN-1097] pytest fixture usage

--- tests/test_dxfeedpy.py
+++ tests/test_dxfeedpy.py
@@ -2,15 +2,30 @@ import dxfeed as dx
 import pytest
 
 
-demo_address = 'demo.dxfeed.com:7300'
-symbols = ['AAPL', 'C']
+@pytest.fixture()
+def connection():
+    # Setup
+    con = dx.dxf_create_connection('demo.dxfeed.com:7300')
+    yield con  # Test here
+    # Teardown
+    dx.dxf_close_connection(con)
 
-def test_connection():
-    con = dx.dxf_create_connection(demo_address)
+
+def test_connection(connection):
     exp_status = 'Connected'
-    act_status = dx.dxf_get_current_connection_status(con)
+    act_status = dx.dxf_get_current_connection_status(connection)
     assert exp_status == act_status
+
+
+def test_connection_address(connection):
+    exp_address = 'demo.dxfeed.com:7300'
+    act_address = dx.dxf_get_current_connected_address(connection)
+    assert exp_address == act_address
+
 
 @pytest.mark.xfail()
 def test_fail_create_subscription_with_no_connection():
     dx.dxf_create_subscription()
+
+# with pytest.raises(TypeError):
+#     pass
