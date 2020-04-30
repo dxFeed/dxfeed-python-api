e81245b599c37f285a0d92596feb41c9bce1fc4a
Ivan Mishalkin <ivan.mishalkin@devexperts.com>
2020-04-30 15:26:13 +0300

    [EN-1328] DXFeed class tests

---
+++ tests/test_dxfeed_class.py
@@ -0,0 +1,33 @@
+import dxfeed as dx
+import pytest
+
+
+class ValueStorage(object):  # config
+    demo_address = 'demo.dxfeed.com:7300'
+
+
+@pytest.fixture(scope='function')
+def dxfeed():
+    dxf = dx.DXFeed(connection_address=ValueStorage.demo_address)
+    yield dxf
+    dxf.close_connection()
+
+
+def test_connection_status_property(dxfeed):
+    expected_status = 'Connected'
+    assert expected_status == dxfeed.connection_status
+
+
+def test_address_property(dxfeed):
+    assert dxfeed.address == ValueStorage.demo_address
+
+
+def test_closing_connection(dxfeed):
+    dxfeed.close_connection()
+    expected_status = 'Not connected'
+    assert expected_status == dxfeed.connection_status
+
+
+@pytest.mark.xfail()
+def test_connection_fail_on_incorrect_address():
+    dxfeed = dx.DXFeed(connection_address='8.8.8.8')
--- tests/test_dxfeedpy.py
+++ tests/test_dxfeedpy.py
@@ -1,5 +1,6 @@
-import dxfeed.core.DXFeedPy as dx
+import dxfeed.core.DXFeedPy as dxс
 import pytest
+
 
 class ValueStorage(object):  # config
     demo_address = 'demo.dxfeed.com:7300'
@@ -10,130 +11,130 @@ class ValueStorage(object):  # config
 @pytest.fixture(scope='function')
 def connection():
     # Setup
-    con = dx.dxf_create_connection(ValueStorage.demo_address)
+    con = dxс.dxf_create_connection(ValueStorage.demo_address)
     yield con  # pass var to test and test itself
     # Teardown
-    dx.dxf_close_connection(con)
+    dxс.dxf_close_connection(con)
 
 
 def test_connection_status(connection):
     exp_status = 'Connected'
-    act_status = dx.dxf_get_current_connection_status(connection)
+    act_status = dxс.dxf_get_current_connection_status(connection)
     assert exp_status == act_status
 
 
 def test_connection_address(connection):
     exp_address = ValueStorage.demo_address
-    act_address = dx.dxf_get_current_connected_address(connection)
+    act_address = dxс.dxf_get_current_connected_address(connection)
     assert exp_address == act_address
 
 
 @pytest.mark.xfail()
 def test_fail_connection_to_wrong_address():
-    dx.dxf_create_connection(address='8.8.8.8')
+    dxс.dxf_create_connection(address='8.8.8.8')
 
 
 @pytest.mark.xfail()
 def test_fail_create_subscription_with_no_connection():
-    dx.dxf_create_subscription()
+    dxс.dxf_create_subscription()
 
 
 @pytest.mark.xfail()
 def test_fail_to_use_subscription_without_connection():
-    connection = dx.dxf_create_connection(ValueStorage.demo_address)
-    sub = dx.dxf_create_subscription(cc=connection, event_type='Trade')
-    dx.dxf_close_connection(connection)
-    dx.dxf_add_symbols(sc=sub, symbols=['AAPL'])
+    connection = dxс.dxf_create_connection(ValueStorage.demo_address)
+    sub = dxс.dxf_create_subscription(cc=connection, event_type='Trade')
+    dxс.dxf_close_connection(connection)
+    dxс.dxf_add_symbols(sc=sub, symbols=['AAPL'])
 
 
 @pytest.mark.parametrize('sub_type', ValueStorage.event_types)
 def test_subscription_on_correct_types(connection, sub_type):
-    sub = dx.dxf_create_subscription(cc=connection, event_type=sub_type)
-    act_sub_type = dx.dxf_get_subscription_event_types(sc=sub)
-    dx.dxf_close_subscription(sub)
+    sub = dxс.dxf_create_subscription(cc=connection, event_type=sub_type)
+    act_sub_type = dxс.dxf_get_subscription_event_types(sc=sub)
+    dxс.dxf_close_subscription(sub)
     assert act_sub_type == sub_type
 
 
 @pytest.mark.parametrize('sub_type', ValueStorage.event_types)
 def test_subscription_timed_on_correct_types(connection, sub_type):
-    sub = dx.dxf_create_subscription_timed(cc=connection, event_type=sub_type, time=0)
-    act_sub_type = dx.dxf_get_subscription_event_types(sc=sub)
-    dx.dxf_close_subscription(sub)
+    sub = dxс.dxf_create_subscription_timed(cc=connection, event_type=sub_type, time=0)
+    act_sub_type = dxс.dxf_get_subscription_event_types(sc=sub)
+    dxс.dxf_close_subscription(sub)
     assert act_sub_type == sub_type
 
 
 @pytest.mark.xfail()
 def test_subscription_fail_on_incorrect_time_type(connection):
-    dx.dxf_create_subscription_timed(cc=connection, event_type='Trade', time=5.2)
+    dxс.dxf_create_subscription_timed(cc=connection, event_type='Trade', time=5.2)
 
 
 @pytest.mark.xfail()
 def test_subscription_fail_on_incorrect_time_value(connection):
-    dx.dxf_create_subscription_timed(cc=connection, event_type='Trade', time=-7)
+    dxс.dxf_create_subscription_timed(cc=connection, event_type='Trade', time=-7)
 
 
 @pytest.mark.xfail()
 def test_subscription_fail_on_incorrect_type(connection):
-    dx.dxf_create_subscription(cc=connection, event_type='TradeQuote')
+    dxс.dxf_create_subscription(cc=connection, event_type='TradeQuote')
 
 
 @pytest.mark.xfail()
 def test_subscription_fail_on_incorrect_type(connection):
-    dx.dxf_create_subscription_timed(cc=connection, event_type='TradeQuote', time=0)
+    dxс.dxf_create_subscription_timed(cc=connection, event_type='TradeQuote', time=0)
 
 
 
 def test_symbol_addition(connection):
     symbols = ['AAPL', 'GOOG']
-    sub = dx.dxf_create_subscription(cc=connection, event_type='Trade')
-    dx.dxf_add_symbols(sc=sub, symbols=symbols)
-    actual_symbols = dx.dxf_get_symbols(sc=sub)
-    dx.dxf_close_subscription(sub)
+    sub = dxс.dxf_create_subscription(cc=connection, event_type='Trade')
+    dxс.dxf_add_symbols(sc=sub, symbols=symbols)
+    actual_symbols = dxс.dxf_get_symbols(sc=sub)
+    dxс.dxf_close_subscription(sub)
     assert symbols == actual_symbols
 
 
 def test_symbol_deletion(connection):
     symbols = ['AAPL', 'GOOG']
-    sub = dx.dxf_create_subscription(cc=connection, event_type='Trade')
-    dx.dxf_add_symbols(sc=sub, symbols=symbols)
-    dx.dxf_remove_symbols(sc=sub, symbols=['AAPL'])
-    actual_symbols = dx.dxf_get_symbols(sc=sub)
-    dx.dxf_close_subscription(sub)
+    sub = dxс.dxf_create_subscription(cc=connection, event_type='Trade')
+    dxс.dxf_add_symbols(sc=sub, symbols=symbols)
+    dxс.dxf_remove_symbols(sc=sub, symbols=['AAPL'])
+    actual_symbols = dxс.dxf_get_symbols(sc=sub)
+    dxс.dxf_close_subscription(sub)
     assert ['GOOG'] == actual_symbols
 
 
 @pytest.mark.filterwarnings('ignore::UserWarning')
 def test_wrong_symbol_types_ignored(connection):
     symbols = ['AAPL', 'GOOG']
-    sub = dx.dxf_create_subscription(cc=connection, event_type='Trade')
-    dx.dxf_add_symbols(sc=sub, symbols=symbols + [1, 5.0, [], True, {}, ()] )
-    actual_symbols = dx.dxf_get_symbols(sc=sub)
-    dx.dxf_close_subscription(sub)
+    sub = dxс.dxf_create_subscription(cc=connection, event_type='Trade')
+    dxс.dxf_add_symbols(sc=sub, symbols=symbols + [1, 5.0, [], True, {}, ()])
+    actual_symbols = dxс.dxf_get_symbols(sc=sub)
+    dxс.dxf_close_subscription(sub)
     assert symbols == actual_symbols
 
 
 def test_symbol_clearing(connection):
     symbols = ['AAPL', 'GOOG']
-    sub = dx.dxf_create_subscription(cc=connection, event_type='Trade')
-    dx.dxf_add_symbols(sc=sub, symbols=symbols)
-    dx.dxf_clear_symbols(sc=sub)
-    actual_symbols = dx.dxf_get_symbols(sc=sub)
-    dx.dxf_close_subscription(sub)
+    sub = dxс.dxf_create_subscription(cc=connection, event_type='Trade')
+    dxс.dxf_add_symbols(sc=sub, symbols=symbols)
+    dxс.dxf_clear_symbols(sc=sub)
+    actual_symbols = dxс.dxf_get_symbols(sc=sub)
+    dxс.dxf_close_subscription(sub)
     assert actual_symbols == []
 
 
 @pytest.mark.parametrize('sub_type', ValueStorage.event_types)
 def test_default_listeners(sub_type):
     # fixture usage causes errors with, probably, threads
-    connection = dx.dxf_create_connection(ValueStorage.demo_address)
-    con_err_status = dx.process_last_error(verbose=False)
-    sub = dx.dxf_create_subscription(connection, sub_type)
-    sub_err_status = dx.process_last_error(verbose=False)
-    dx.dxf_attach_listener(sub)
-    add_lis_err_status = dx.process_last_error(verbose=False)
-    dx.dxf_detach_listener(sub)
-    drop_lis_err_status = dx.process_last_error(verbose=False)
-    dx.dxf_close_subscription(sub)
-    dx.dxf_close_connection(connection)
+    connection = dxс.dxf_create_connection(ValueStorage.demo_address)
+    con_err_status = dxс.process_last_error(verbose=False)
+    sub = dxс.dxf_create_subscription(connection, sub_type)
+    sub_err_status = dxс.process_last_error(verbose=False)
+    dxс.dxf_attach_listener(sub)
+    add_lis_err_status = dxс.process_last_error(verbose=False)
+    dxс.dxf_detach_listener(sub)
+    drop_lis_err_status = dxс.process_last_error(verbose=False)
+    dxс.dxf_close_subscription(sub)
+    dxс.dxf_close_connection(connection)
     assert (con_err_status, sub_err_status, add_lis_err_status, drop_lis_err_status) == (0, 0, 0, 0)
 
