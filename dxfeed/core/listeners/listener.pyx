4ea17bf675bcd50e44b34bd5e28e02d76a194208
Ivan Mishalkin <ivan.mishalkin@devexperts.com>
2020-05-06 16:26:09 +0300

    [EN-1450] exchange code for trades corrected

--- dxfeed/core/listeners/listener.pyx
+++ dxfeed/core/listeners/listener.pyx
@@ -15,14 +15,14 @@ cdef void trade_default_listener(int event_type,
                                  dxf_const_string_t symbol_name,
                                  const dxf_event_data_t*data,
                                  int data_count, void*user_data) nogil:
-    cdef dxf_trade_t*trades = <dxf_trade_t*> data
+    cdef dxf_trade_t* trades = <dxf_trade_t*> data
     with gil:
         py_data = <object> user_data
 
         for i in range(data_count):
             py_data.safe_append([unicode_from_dxf_const_string_t(symbol_name),
                                  trades[i].price,
-                                 trades[i].exchange_code,
+                                 unicode_from_dxf_const_string_t(&trades[i].exchange_code),
                                  trades[i].size,
                                  trades[i].tick,
                                  trades[i].change,
