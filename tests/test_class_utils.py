241245be725fc5cdfb5ec509a88990eea9ccf757
Ivan Mishalkin <ivan.mishalkin@devexperts.com>
2020-05-25 13:11:47 +0300

    [EN-1328] to_iterable_of_strings tests added

--- tests/test_class_utils.py
+++ tests/test_class_utils.py
@@ -1,7 +1,19 @@
 from dxfeed.wrappers import class_utils as cu
+import pytest
 
 
 def test_to_iterable_of_strings_with_string():
     symbol = 'AAPL'
     actual_symbol = cu.to_iterable_of_strings(symbol)
     assert actual_symbol == [symbol]
+
+
+def test_to_iterable_of_strings_with_iterable():
+    symbols = ('AAPL', 'MSFT')
+    actual_symbols = cu.to_iterable_of_strings(symbols)
+    assert actual_symbols == symbols
+
+
+@pytest.mark.xfail
+def test_to_iterable_of_strings_with_incorrect_type():
+    cu.to_iterable_of_strings(123)
