4ea1fb9b0f12dace762b79b733dead8820de9f87
Ivan Mishalkin <ivan.mishalkin@devexperts.com>
2020-06-25 18:45:20 +0300

    [EN-1712] python 3.8 support added

--- dxfeed/__init__.py
+++ dxfeed/__init__.py
@@ -1,6 +1,11 @@
 from dxfeed.wrappers.endpoint import Endpoint
 from dxfeed.core.utils.handler import EventHandler, DefaultHandler
-import importlib_metadata
+try:
+    from importlib_metadata import version
+except ModuleNotFoundError:
+    # Python 3.8
+    from importlib.metadata import version
 
 
-__version__ = importlib_metadata.version('dxfeed')
+__version__ = version('dxfeed')
+__all__ = ['Endpoint', 'EventHandler', 'DefaultHandler']
