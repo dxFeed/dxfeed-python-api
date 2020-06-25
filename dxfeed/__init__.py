3878c8345076d19aef3b7290c91a72bd58107d2d
Ivan Mishalkin <ivan.mishalkin@devexperts.com>
2020-06-25 18:39:13 +0300

    [EN-1712] __version__ added

--- dxfeed/__init__.py
+++ dxfeed/__init__.py
@@ -1,2 +1,6 @@
 from dxfeed.wrappers.endpoint import Endpoint
 from dxfeed.core.utils.handler import EventHandler, DefaultHandler
+import importlib_metadata
+
+
+__version__ = importlib_metadata.version('dxfeed')
