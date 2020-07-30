4dfa9f5830500afb318647d18ad1d4f19bc70617
Ivan Mishalkin <ivan.mishalkin@devexperts.com>
2020-07-30 14:35:30 +0300

    [EN-1712] toml import

--- dxfeed/__init__.py
+++ dxfeed/__init__.py
@@ -1,11 +1,12 @@
 from dxfeed.wrappers.endpoint import Endpoint
 from dxfeed.core.utils.handler import EventHandler, DefaultHandler
 import pkg_resources
+import toml
+from pathlib import Path
+
 
 try:
     __version__ = pkg_resources.get_distribution('dxfeed').version
 except pkg_resources.DistributionNotFound:
-    import toml
-    from pathlib import Path
-    pyproject = toml.load(Path(__file__).parents[1].joinpath('pyproject.toml'))
+    pyproject = toml.load(Path(__file__).parents[1] / 'pyproject.toml')
     __version__ = pyproject['tool']['poetry']['version']
