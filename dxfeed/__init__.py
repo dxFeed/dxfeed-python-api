06c1118ea05c66087b5263371ab6c30edff7d0a6
Ivan Mishalkin <ivan.mishalkin@devexperts.com>
2020-07-30 11:52:03 +0300

    [EN-1712] version for dev environment

--- dxfeed/__init__.py
+++ dxfeed/__init__.py
@@ -2,5 +2,10 @@ from dxfeed.wrappers.endpoint import Endpoint
 from dxfeed.core.utils.handler import EventHandler, DefaultHandler
 import pkg_resources
 
-
-__version__ = pkg_resources.get_distribution('dxfeed').version
+try:
+    __version__ = pkg_resources.get_distribution('dxfeed').version
+except pkg_resources.DistributionNotFound:
+    import toml
+    from pathlib import Path
+    pyproject = toml.load(Path(__file__).parents[1].joinpath('pyproject.toml'))
+    __version__ = pyproject['tool']['poetry']['version']
