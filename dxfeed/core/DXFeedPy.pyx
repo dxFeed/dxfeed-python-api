5fb6844a4f40117ca333026ef862ee874599c75a
Ivan Mishalkin <ivan.mishalkin@devexperts.com>
2020-08-04 14:19:36 +0300

    [EN-1818] better warning message

--- dxfeed/core/DXFeedPy.pyx
+++ dxfeed/core/DXFeedPy.pyx
@@ -306,7 +306,9 @@ def dxf_attach_listener(SubscriptionClass sc):
     event_handler = sc.get_event_handler()
     if not event_handler:
         raise ValueError('Event handler is not defined!')
-    warn(FutureWarning('New order in event fields will be introduced in dxfeed-0.6.0. Address event fields by name.'))
+    warn('New order in event fields will be introduced in dxfeed-0.6.0. Please, address event fields '
+         'by name in custom event handler.',
+         FutureWarning)
 
     if sc.event_type_str == 'Trade':
         event_handler.columns = lis.TRADE_COLUMNS
