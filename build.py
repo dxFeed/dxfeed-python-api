d2a89d1eef4856b0e02c148343dbf3d952aaf196
Anatoly Kalin <ttldtor@gmail.com>
2021-08-29 13:00:22 +0300

    [EN-3260] install_name_tool

--- build.py
+++ build.py
@@ -95,7 +95,8 @@ if platform.system() == 'Windows':
     extra_link_args = None
 elif platform.system() == 'Darwin':
     runtime_library_dirs = ['.', str(capi_bin_dir)]
-    extra_link_args = ['-Wl,-rpath,@executable_path', '-Wl,-rpath,@loader_path/dxfeed/core', f'-Wl,-rpath,.', f'-Wl,-rpath,{str(capi_bin_dir)}']
+    extra_link_args = [f'-Wl,-rpath,.', f'-Wl,-rpath,{str(capi_bin_dir)}']
+    run(('install_name_tool', '-id', f'@rpath/{capi_library_file_name}', capi_library_file_name))
 else:
     runtime_library_dirs = ['$ORIGIN', '.', str(capi_bin_dir)]
     extra_link_args = None
