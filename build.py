248d600f620042b22145e349acc42a245df48d63
Anatoly Kalin <ttldtor@gmail.com>
2021-08-29 13:51:44 +0300

    [EN-3260] test 8.3.1

--- build.py
+++ build.py
@@ -94,8 +94,8 @@ if platform.system() == 'Windows':
     runtime_library_dirs = None
     extra_link_args = None
 elif platform.system() == 'Darwin':
-    runtime_library_dirs = ['.', str(capi_bin_dir)]
-    extra_link_args = [f'-Wl,-rpath,.', f'-Wl,-rpath,{str(capi_bin_dir)}']
+    runtime_library_dirs = ['@loader_path', '.', str(capi_bin_dir)]
+    extra_link_args = ['-Wl,-rpath,@loader_path', '-Wl,-rpath,.', f'-Wl,-rpath,{str(capi_bin_dir)}']
     # run(('install_name_tool', '-id', f'@rpath/{capi_library_file_name}', capi_library_file_name))
 else:
     runtime_library_dirs = ['$ORIGIN', '.', str(capi_bin_dir)]
